0daa516c55a9883f6efd88f67435c25c8630a769
Alexander Rippe <duwar95@gmail.com>
2025-10-13 15:11:44 -0500

    [creación y diseño de Navbar]

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +8,12 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
-export class Navbar {
+export class Navbar implements OnInit {
+  baseUrl: string = environment.appUrl;
+  logoPath: string = '/uploads/logo.png';
+  logoUrl: string = '';
 
+  ngOnInit() {
+    this.logoUrl = `${this.baseUrl}${this.logoPath}`;
+  }
 }
