d759df511182f607d241c5b2247bdf4a400fd94c
Alexander Rippe <duwar95@gmail.com>
2025-10-13 16:18:02 -0500

    coomit - [Creación de Register-form y Login-form]

--- src/app/components/register-form/register-form.ts
+++ src/app/components/register-form/register-form.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-register-form',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './register-form.html',
   styleUrl: './register-form.css'
 })
