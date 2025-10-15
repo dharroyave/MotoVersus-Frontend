ddcaf64c60ec965e03a3c1819754f9bf069188f7
dharroyave <duvan.arroyave03@gmail.com>
2025-10-14 20:54:33 -0500

    Creacion Service Comments

--- src/app/services/comments.ts
+++ src/app/services/comments.ts
@@ -1,8 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Comments } from '../interfaces/comments';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class Comments {
-  
+export class CommentsService {
+  private _httpClient = inject(HttpClient);
+  private apiUrl = environment.appUrl;
+
+  // metodos para hacer las peticiones a la api
+
+  //peticion post
+  postComments(commentsToCreate: Comments) {
+    return this._httpClient.post(`${this.apiUrl}/comments/crear`, commentsToCreate);
+  }
+
+  //peticion get
+  getComments() {
+    return this._httpClient.get(`${this.apiUrl}/comments/mostrar`);
+  }
+
+  // peticion put
+  putComments(commentsToUpdate: Comments, id: string) {
+    return this._httpClient.put(`${this.apiUrl}/comments/actualizar/${id}`, commentsToUpdate);
+  }
+
+  // peticion delete
+  deleteComments(id: string) {
+    return this._httpClient.delete(`${this.apiUrl}/comments/eliminar/${id}`);
+  }
 }
