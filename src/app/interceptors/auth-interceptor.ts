1c42327c3984fac693740b27777cfa30e239acef
nicolcortes-angel <nicolmurciacortes7@gmail.com>
2025-10-20 01:28:54 -0500

    interceptors

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -1,5 +1,17 @@
+//Nos permite interceptar el token al iniciar sesion se lo pasa a cada peticion que lo necesita 
+
 import { HttpInterceptorFn } from '@angular/common/http';
+import { LoginService } from '../services/login';
+import { inject } from '@angular/core';
+
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const _loginService = inject(LoginService);
+  const token = _loginService.getToken();
+
+  const request = token ? req.clone({setHeaders: { Authorization: `Bearer + token}` }}) : req;
+
   return next(req);
 };
+
