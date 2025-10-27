0ea2f2eaf135494a3d7d982b5b7eb57e8b8c4601
Alexander Rippe <duwar95@gmail.com>
2025-10-26 22:04:27 -0500

    commit [Anexo de conexion del backend para pagina de login y register]

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit , inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { environment } from '../../../environments/environment';
+import { LoginService } from '../../services/login';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
@@ -12,6 +14,12 @@ export class Navbar implements OnInit {
   baseUrl: string = environment.appUrl;
   logoPath: string = '/uploads/logo.png';
   logoUrl: string = '';
+
+  private _loginService = inject(LoginService);
+
+  get isvisible(): boolean {
+    return this._loginService.isloggedIn() && this._loginService.isAdmin();
+  }
 
   ngOnInit() {
     this.logoUrl = `${this.baseUrl}${this.logoPath}`;
--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,21 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators, ɵInternalFormsSharedModule } from '@angular/forms';
+import { User } from '../../interfaces/user';
+import Swal from 'sweetalert2';
+import { UserService } from '../../services/users';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule],
+  imports: [ɵInternalFormsSharedModule, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
 
-  registerForm = new FormGroup({
-    nombre: new FormControl(''),
-    correo: new FormControl(''),
-    numero: new FormControl<number | null>(null),
-    fotoPerfil: new FormControl(''),
-    contrasena: new FormControl('')
-  });
+  private _userService = inject(UserService);
+  private _router = inject(Router);
 
+  registerForm = new FormGroup({
+    nombre: new FormControl('', [Validators.required]),
+    apellido: new FormControl('', [Validators.required]),
+    user: new FormControl('', [Validators.required]),
+    correo: new FormControl('', [Validators.required, Validators.email]),
+    numero: new FormControl<string>('', [Validators.required, Validators.pattern('^[0-9]+$')]),
+    fotoPerfil: new FormControl('', [Validators.required]),
+    contrasena: new FormControl<string>('', [Validators.required, Validators.minLength(6)])
+  });
+  handleSubmit() {
+
+    const userData: User = {
+      nombre: this.registerForm.value.nombre || '',         
+      apellido: this.registerForm.value.apellido || '',    
+      user: this.registerForm.value.user || '',    
+      correo: this.registerForm.value.correo || '',         
+      numero: Number(this.registerForm.value.numero) || 0,       
+      fotoPerfil: this.registerForm.value.fotoPerfil || '',      
+      contrasena: this.registerForm.value.contrasena || '',   
+      role: 'user'
+    };
+
+    console.log("Datos del usuario a registrar:", userData);
+
+    this._userService.postUser(userData).subscribe({
+      next: (res: any) => {
+        console.log("✅ Respuesta del servidor:", res);
+        Swal.fire({
+          icon: "success",
+          title: "¡Registro exitoso!",
+          text: res.mensaje
+        });
+        this.registerForm.reset(); // opcional, limpia el formulario
+      },
+      error: (err: any) => {
+        console.error("Error en el registro:", err);
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: err.error?.mensaje || "Ocurrió un error al registrar el usuario"
+        });
+      }
+    });
+  }
 }
