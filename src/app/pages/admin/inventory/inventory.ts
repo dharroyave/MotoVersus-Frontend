8012b55ab5d41edab24a15ef44224d9c366a28bb
dharroyave <duvan.arroyave03@gmail.com>
2025-10-27 20:52:50 -0500

    Actualizacion Duvan

---
+++ src/app/pages/admin/inventory/inventory.ts
@@ -0,0 +1,16 @@
+import { Component, inject, OnInit } from '@angular/core';
+import {ProductService} from '../../../services/products';
+import { Product } from '../../../interfaces/product';
+import swal from 'sweetalert2';
+
+
+
+@Component({
+  selector: 'app-inventory',
+  imports: [],
+  templateUrl: './inventory.html',
+  styleUrl: './inventory.css'
+})
+export class Inventory {
+
+}
